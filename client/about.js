49411b79def1190e7f546aff702c524c57365b2e
Anton Savoskin <anton@sliday.com>
2016-06-17 10:13:19 +0700

    Change About page

--- client/about.js
+++ client/about.js
@@ -3,14 +3,17 @@ var React = require('react')
 var About = React.createClass({
   render: function () {
     return <div className="about">
-      <h1>This is the Hexo Admin Plugin</h1>
-      <p><strong>Goal: Provide an awesome admin experience for managing your blog.</strong></p>
+      <h1>How to create a new post</h1>
       <p>
-        Useful links:
-        <ul>
-          <li><a href="http://hexo.io">Hexo site</a></li>
-          <li><a href="https://github.com/jaredly/hexo-admin-plugin">Github page for this plugin</a></li>
-        </ul>
+        <ol>
+          <li>Click "Posts", then "+ New Post". Enter post title. Click blue checkmark. It opens post edit page.</li>
+          <li>Enter post text, using markdown.
+            <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">Markdown Cheatsheet</a>
+          </li>
+          <li>If post need images, follow this. Open image in image viewer. Press Ctrl+C. Then press Ctrl+V in post text. It pasts link to post and upload image data to server. This feature works in Chrome, not in Firefox.</li>
+          <li>Click "Publish", then "Preview" link.</li>
+          <li>If it looks good, click "Deploy" in menu, enter message, and click "Deploy" button. This generate new site and save backup to version manager.</li>
+        </ol>
       </p>
     </div>
   }
